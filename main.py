0a67d8d9107cc597d134bcb097b35aa9561619c8
ozx_og <53302924+OZX-OG@users.noreply.github.com>
2022-09-06 03:24:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import os, time; x = 0
-for i in range(101):os.system("cls"); print(f"Starting Hack{x * '.'}\nHacking FBI and Nasa {i}%"); x = 0 if x==3 else x + 1; time.sleep(0.25)
+for i in range(101):os.system("cls"); print(f"Starting Hack{x * '.'}\nHacking FBI and Nasa {i}%"); x = 0 if x==3 else x + 1; time.sleep(0.05)
+print("FBI and Nasa Hacked Successfully")
